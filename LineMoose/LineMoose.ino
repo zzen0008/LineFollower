61391f3502d3877a43852839112ac7b482f5b245
Prying <>
2025-04-30 15:13:03 +1000

    The even better stuff

--- LineMoose/LineMoose.ino
+++ LineMoose/LineMoose.ino
@@ -2,15 +2,23 @@
 
 #include <Pololu3piPlus32U4.h>
 
-#define FORWARD_CHAR  'f'
-#define BACKWARD_CHAR 'b'
-#define LEFT_CHAR     'l'
-#define RIGHT_CHAR    'r'
-#define AUTO_CHAR     'a'
+const char sheet[] PROGMEM =
+	"v12 L16 o4 c8.e8f#8ag8.e8c8<a<f#<f#<f#<g";
+
+#define FORWARD_CHAR    'f'
+#define BACKWARD_CHAR   'b'
+#define LEFT_CHAR       'l'
+#define RIGHT_CHAR      'r'
+#define AUTO_CHAR       'a'
+#define ANOY_D_BOY_CHAR 'm'
 
 #define DEFAULT_SPEED 50
 
-LineSensor lineSensor;
+#define ECHO_DEBUG true
+
+using namespace Pololu3piPlus32U4;
+
+LineSensors lineSensor;
 Motors motor;
 Buzzer buzzer;
 
@@ -19,7 +27,12 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
 
-  buzzer.playMode(PLAY_CHECK);
+  //buzzer.playMode(PLAY_CHECK);
+  //buzzer.playFromProgramSpace(sheet);
+
+  while (ECHO_DEBUG) {
+    echoServer();
+  }
 }
 
 void loop() {
@@ -29,23 +42,23 @@ void loop() {
   switch (getNextChar()) {
     case FORWARD_CHAR:
       // Set motors forward
-      motor.setSpeed(DEFAULT_SPEED, DEFUALT_SPEED);
+      motor.setSpeeds(DEFAULT_SPEED, DEFAULT_SPEED);
       break;
     
     case BACKWARD_CHAR:
-      motor.setSpeed(-DEFAULT_SPEED, -DEFUALT_SPEED);
+      motor.setSpeeds(-DEFAULT_SPEED, -DEFAULT_SPEED);
       break;
 
     case LEFT_CHAR:
-      motor.setSpeed(-DEFAULT_SPEED, DEFUALT_SPEED);
+      motor.setSpeeds(-DEFAULT_SPEED, DEFAULT_SPEED);
       break;
 
     case RIGHT_CHAR:
-      motor.setSpeed(DEFAULT_SPEED, -DEFUALT_SPEED);
+      motor.setSpeeds(DEFAULT_SPEED, -DEFAULT_SPEED);
       break;
 
     case ANOY_D_BOY_CHAR:
-      musicFlag = true;
+      buzzer.playFromProgramSpace(sheet);
       break;
 
     case AUTO_CHAR:
@@ -53,7 +66,7 @@ void loop() {
       break;
 
     default:
-      motor.setSpeed(0,0);
+      motor.setSpeeds(0,0);
       break;
   }
 
@@ -63,8 +76,14 @@ void loop() {
 
 char getNextChar(){
   // Check if anything is in the serial buffer
-  if (Serial.avalible()) {
+  if (Serial.available()) {
     return Serial.read();
+  }
+}
+
+void echoServer(){
+  if (Serial.available()) {
+    Serial.write(Serial.read());
   }
 }
 
